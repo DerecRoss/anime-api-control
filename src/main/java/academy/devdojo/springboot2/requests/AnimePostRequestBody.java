8f275687629d9e4d48c1c06a1cbe8b35e17fe66e
DerecRoss <derecdz999@gmail.com>
2025-05-09 22:04:06 -0300

    feat: add WebMvcConfigure to Pageable

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,10 +1,12 @@
 package academy.devdojo.springboot2.requests;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AnimePostRequestBody {
     @NotEmpty(message = "The name of anime is empty")
+    @NotNull
     private String name;
 }
