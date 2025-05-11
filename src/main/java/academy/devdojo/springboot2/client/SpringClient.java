86da3311ad66ba8fac2b4443e670f07dfb288707
DerecRoss <derecdz999@gmail.com>
2025-05-10 21:15:53 -0300

    feat: RestTemplateCrud in Client using Client of my API

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -3,12 +3,12 @@ package academy.devdojo.springboot2.client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @Log4j2
 public class SpringClient {
@@ -31,5 +31,26 @@ public class SpringClient {
 
         log.info(exchange.getBody());
 
+//        Anime kingdom = Anime.builder().name("Kingdom").build();
+//        Anime anime = new RestTemplate().postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+//        log.info("saved anime '{}'", anime);
+        Anime anime = Anime.builder().name("Samurai X").build();
+        ResponseEntity<Anime> animeSaved = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.POST, new HttpEntity<>(anime, createJsonHeader()), Anime.class);
+        log.info("saved anime '{}'", animeSaved);
+
+        Anime animeToUpdate = animeSaved.getBody();
+        animeToUpdate.setName("Dororo 2");
+
+        ResponseEntity<Void> animeUpdated = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.PUT, new HttpEntity<>(animeToUpdate, createJsonHeader()), Void.class);
+        log.info("updated anime '{}'", animeUpdated);
+
+        ResponseEntity<Void> animeDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}", HttpMethod.DELETE, null, Void.class, animeToUpdate.getId());
+        log.info("deleted anime '{}'", animeDeleted);
+
+    }
+    private static HttpHeaders createJsonHeader(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
