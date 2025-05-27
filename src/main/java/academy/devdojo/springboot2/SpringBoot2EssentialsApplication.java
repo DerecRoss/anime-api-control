2b843deb393a756a51fcb082e3d5671168ef5bcb
DerecRoss <derecdz999@gmail.com>
2025-05-27 13:40:57 -0300

    feat: add grafana for view metrics with prometheus data source

--- src/main/java/academy/devdojo/springboot2/SpringBoot2EssentialsApplication.java
+++ src/main/java/academy/devdojo/springboot2/SpringBoot2EssentialsApplication.java
@@ -1,7 +1,11 @@
 package academy.devdojo.springboot2;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBoot2EssentialsApplication {
@@ -10,4 +14,9 @@ public class SpringBoot2EssentialsApplication {
 		SpringApplication.run(SpringBoot2EssentialsApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
